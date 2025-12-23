b211c128db90702456935f707e16dc281e91a168
siprasanghamitradash <dash.sipra.sanghamitra@gmail.com>
2025-12-23 23:12:03 +0530

    first commit

--- try.py
+++ try.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello! This is my first python website.</h1>"
+    return "<h1>Hello! This is my first and Flask website.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
