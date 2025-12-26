291c8835c1b4f38d53999a285aeb9288c009f635
siprasanghamitradash <dash.sipra.sanghamitra@gmail.com>
2025-12-26 23:26:38 +0530

    minor changes

--- try.py
+++ try.py
@@ -1,15 +1,9 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from groq import Groq
-from dotenv import load_dotenv
 from savedapi import GROQ_API_KEY
-load_dotenv()
 
 app = Flask(__name__)
-# client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
-# Skip the .env loading and hardcode the key directly
-# Replace 'gsk_...' with your actual key from Groq Cloud
 client = Groq(api_key=GROQ_API_KEY)
 
 @app.route('/')
@@ -21,7 +15,6 @@ def chat():
     user_message = request.json.get("message")
     
     try:
-        # We use 'llama-3.3-70b-versatile' - it's very smart for code
         completion = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": user_message}]
